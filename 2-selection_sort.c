6727b7d91314a191e7d8f768903a7ef238afd530
paschalugwu <ugwupaschal@gmail.com>
2023-12-15 08:32:24 +0100

    feat(sort): implement efficient selection sort algorithm
    Refactored the selection_sort function to efficiently sort an array of integers in ascending order using the Selection sort algorithm. Introduced a separate swap function for improved code organization and readability. The print_array function is called after each swap to display the array's current state.

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * swap - swap two numbers
+ * @a: integer
+ * @b: integer
+*/
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: array to sort
+ * @size: size of array
+*/
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		swap(&array[i], &array[min]);
+		print_array(array, size);
+	}
+}
