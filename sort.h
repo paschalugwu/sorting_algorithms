dbc3b9502416b5ec6d0462e10b107fadbba01999
paschalugwu <ugwupaschal@gmail.com>
2023-12-16 11:32:33 +0100

    feat(sort): implement cocktail_sort_list for doubly linked list
    Add the cocktail_sort_list function to efficiently sort a doubly linked list of integers in ascending order. The function utilizes swap_node_ahead and swap_node_behind for node swapping during the sorting process. Each swap is followed by printing the updated list for visualization.

--- sort.h
+++ sort.h
@@ -34,5 +34,8 @@ void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
 void swap_ints(int *a, int *b);
 void shell_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* _SORT_H_ */
