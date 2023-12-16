0a5c339bed8cb654530224107018405e0b0548e7
paschalugwu <ugwupaschal@gmail.com>
2023-12-16 17:24:34 +0100

    refactor(sort): Implement Counting sort algorithm
    Refactor the code to implement the Counting sort algorithm in the  counting_sort  function. The function takes an array and its size as parameters.
    
    The  get_max  function is implemented to find the maximum value in the array. It iterates through the array and updates the maximum value if a larger value is found.
    
    The  counting_sort  function allocates memory for the  sorted  and  count  arrays using  malloc . It then initializes the  count  array with zeros and counts the occurrences of each element in the  array . The cumulative sum of the  count  array is calculated and printed using the  print_array  function.
    
    The  array  is sorted by placing each element in its correct position in the  sorted  array based on the values in the  count  array. Finally, the sorted elements are copied back to the original  array .
    
    The memory allocated for the  sorted  and  count  arrays is freed using  free .

--- sort.h
+++ sort.h
@@ -37,5 +37,7 @@ void shell_sort(int *array, size_t size);
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 void cocktail_sort_list(listint_t **list);
+int get_max(int *array, int size);
+void counting_sort(int *array, size_t size);
 
 #endif /* _SORT_H_ */
